00bc728daa66bf0a2d4ea7f3059605240b48b2e2
Your Name <you@example.com>
2025-01-14 20:09:29 +0100

    asdasd

--- app/vip/page.tsx
+++ app/vip/page.tsx
@@ -14,7 +14,7 @@ import { ethers } from 'ethers';
 import { ClockIcon, CalendarIcon } from '@heroicons/react/24/outline';
 
 
-
+//niceone
 
 // Define BudgetState interface
 interface BudgetState {
