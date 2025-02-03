2dc301c210dbca5d7d1fdb67ca2311f5e7fce10a
Your Name <you@example.com>
2025-02-03 14:23:17 +0100

    asas

--- app/addy/utils/formatCurrency.ts
+++
@@ -1,8 +0,0 @@
-// app/advanced/utils/formatCurrency.ts
-export const formatCurrency = (value: number, currency: string) => {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: currency === "NGN" ? "NGN" : currency,
-      minimumFractionDigits: 0,
-    }).format(value);
-  };
